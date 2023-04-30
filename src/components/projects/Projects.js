c2291b9b0ee50c458516fb08f31b1d630124c79d
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-04-30 23:40:23 +0700

    update project

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,62 +1,80 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "../ProjectCard";
-import { projects }   from "./data";
+import { Jobs, University, Training } from "./data";
 import colorSharp2 from "../../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-
   return (
     <section className="project" id="projects">
       <Container>
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects & Work experience</h2>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Jobs</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">University</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Training</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects & Work experience</h2>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Jobs</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">University</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Training</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {Jobs.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {University.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <Row>
+                          {Training.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2} alt="colorSharp"></img>
+      <img
+        className="background-image-right"
+        src={colorSharp2}
+        alt="colorSharp"
+      ></img>
     </section>
-  )
-}
+  );
+};
--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -2,35 +2,42 @@ import projImg1 from '../../assets/img/project-img1.png'
 import projImg2 from '../../assets/img/project-img2.png'
 import projImg3 from '../../assets/img/project-img3.png'
 
-export const projects = [
+export const Jobs = [
   {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg1,
+    title: "System Engineer",
+    description: "Oct 2021- Sep2022",
+    imgUrl: "https://s3-symbol-logo.tradingview.com/somboon-advance-technology--600.png",
   },
   {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg2,
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg3,
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg1,
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg2,
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg3,
-  },
+    title: "Front-end Engineer",
+    description: "Sep 2022 - Currently",
+    imgUrl: "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/gjv8r9gmf4qbnfsbgytn",
+  }
 ];
+
+export const University = [
+  {
+    title: "Somboon Advance Technology Co., Ltd (SAT)",
+    description: "System Engineer",
+    imgUrl: "https://www.satpcl.co.th/themes/default/assets/static/images/logo.png",
+  },
+  {
+    title: "Mungmee Ecommerce Co., Ltd",
+    description: "Front-end Engineer",
+    imgUrl: "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/gjv8r9gmf4qbnfsbgytn",
+  }
+];
+
+export const Training = [
+  {
+    title: "Somboon Advance Technology Co., Ltd (SAT)",
+    description: "System Engineer",
+    imgUrl: "https://www.satpcl.co.th/themes/default/assets/static/images/logo.png",
+  },
+  {
+    title: "Mungmee Ecommerce Co., Ltd",
+    description: "Front-end Engineer",
+    imgUrl: "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/gjv8r9gmf4qbnfsbgytn",
+  }
+];
+
