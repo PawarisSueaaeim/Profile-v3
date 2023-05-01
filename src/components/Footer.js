3f4832f08415c87f15605d8225cf341a35fe6e02
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-05-01 23:06:24 +0700

    clear up code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
@@ -9,12 +8,11 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <Row className="align-items-center">
-          <MailchimpForm />
+        <Row className="align-items-center mt-5">
           <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
+          <Col size={12} sm={6} className="text-center text-sm-end mt-5">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/pleum-pawaris-rabeab-234292253/"><img src={navIcon1} alt="Icon" /></a>
               <a href="https://web.facebook.com/profile.php?id=100006235390790"><img src={navIcon2} alt="Icon" /></a>
