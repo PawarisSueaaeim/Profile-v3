5e26e85411a364d0530c17fcdb3114e66380eca3
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-09-23 23:55:17 +0700

    edit style

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ imgUrl,company, position,description }) => {
+export const ProjectCard = ({ imgUrl,company, position, description }) => {
   return (
     <React.Fragment>
       <Col size={12} sm={6} md={4}>
         <div className="proj-imgbx">
-          <img src={imgUrl} alt="imgUrl" />
+          <img src={imgUrl} alt="imgUrl" style={{ objectFit: 'cover' }}/>
           <div className="proj-txtx">
             <h4>{company}</h4>
             <span>{position}</span>
